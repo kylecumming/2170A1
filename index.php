5aaeac0652a3838307710f209ec5549a96c7f178
Chris Maxwell <maxwell@helpdesk.cs.dal.ca>
2021-01-13 10:13:55 -0400

    Starter Code

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+/*
+ * CSCI 2170: ONLINE EDITION, WINTER 2021
+ * STARTER CODE FOR ASSIGNMENT 1
+ * 
+ * This code was customized by Dr. Raghav V. Sampangi (raghav@cs.dal.ca)
+ * using the code from the Bootstrap "Album" example available at:
+ * https://getbootstrap.com/docs/5.0/examples/album/ (accessed 11 Jan 2021)
+ * (Created by: Mark Otto, Jacob Thornton, and Bootstrap contributors)
+ * The Album example is (c) Bootstrap, and is free to download and customize.
+ *
+ */
+?><!doctype html>
+<html lang="en">
+  	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Jedi Email - Back to Basics</title>
+	
+		<!-- Bootstrap core CSS -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">	
+  	</head>
+  	<body>	
+		<header>
+			<nav class="navbar navbar-expand navbar-dark bg-dark" aria-label="Second navbar example">
+				<div class="container-fluid">
+					<a class="navbar-brand" href="#">JediMail</a>
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+				<div class="collapse navbar-collapse" id="navbarsExample02">
+					<ul class="navbar-nav me-auto">
+						<li class="nav-item active">
+							<a class="nav-link" aria-current="page" href="#">Home</a>
+						</li>
+					</ul>
+					<form>
+						<input class="form-control" type="text" placeholder="Search" aria-label="Search">
+					</form>
+				</div>
+				</div>
+			</nav>
+		</header>
+
+		<main>
+			<div class="py-5 text-center container">
+				<div class="row py-lg-5">
+					<div class="col-lg-6 col-md-8 mx-auto">
+						<h1 class="fw-light">JediMail</h1>
+						<p class="lead text-muted">Sometimes, the email interface simply needs to be a simple one. For what the Jedi do, we start here with a simple, no clutter, email interface.</p>
+					</div>
+				</div>
+			</div>
+
+			<!-- ADD YOUR CODE HERE -->
+
+		</main>
+
+		<footer class="text-muted py-5">
+			<div class="container">
+				<p class="float-end mb-1">
+					<a href="#">Back to top</a>
+				</p>
+				<p class="mb-1">&copy; 2021. JediMail: The Force guided back to basics email interface.</p>
+			</div>
+		</footer>
+
+		<!-- Bootstrap core JS -->
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+	  
+	</body>
+</html>
